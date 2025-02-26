3fd9cc6725ae6d5a0b9dddeb8dda869ea7694946
Olga-dot-art <116544878+Olga-dot-art@users.noreply.github.com>
2025-02-26 19:23:40 +0900

    Add GraphQL.

---
+++ src/graphql/data.ts
@@ -0,0 +1,24 @@
+export const db = [
+    {
+      zip: "96815",
+      weather: "sunny",
+      tempC: "25C",
+      tempF: "70F",
+      friends: [{zip:"96814", weather:"rain"}, {zip:"96826", weather:"sunny"}],
+    },
+    {
+      zip: "96826",
+      weather: "sunny",
+      tempC: "30C",
+      tempF: "86F",
+      friends: [{zip:"96814", weather:"cloudy"}, {zip:"96816", weather:"rain"}],
+    },
+    {
+      zip: "96814",
+      weather: "sunny",
+      tempC: "20C",
+      tempF: "68F",
+      friends: [{zip:"96824", weather:"rain"}, {zip:"96826", weather:"sunny"}],
+    },
+  ];
+  
---
+++ src/graphql/schema.ts
@@ -0,0 +1,31 @@
+import  gql  from  "graphql-tag";
+
+export const typeDefs = gql`
+  type FriendsType {
+    zip: String!
+    weather: String!
+  }
+  type LocationWeatherType {
+    zip: String!
+    weather: String!
+    tempC: String!
+    tempF: String!
+    friends: [FriendsType]!
+  }
+
+  input LocationWeatherInput {
+    zip: String!
+    weather: String
+    tempC: String
+    tempF: String
+    friends: [String]
+  }
+
+  type Query {
+    weather(zip: String): [LocationWeatherType]!
+  }
+
+  type Mutation {
+    weather(data: LocationWeatherInput): [LocationWeatherType]!
+  }
+`;
